69d8cc247152019a88c9d995367ea6d5f032b5ae
1234 <arpitsinghthakur321@gmail.com>
2023-12-30 21:21:53 +0530

    final 5.1

--- class5.1/vite-project/src/App.jsx
+++ class5.1/vite-project/src/App.jsx
@@ -11,19 +11,45 @@ function App() {
   //   )
   // }
 
+  const [Value, setValue] = useState(0);
+
+
 
   return (
     <>
       <div className="card">
         {/* <button onClick={changeValue}>
-          count is {count}
-        </button> */}
+          count is {count} 
+        </button>
 
-Hii There
+Hii There */}
+
+        <ButtonChange Value={Value} setValue={setValue} ></ButtonChange>
+
 
       </div>
     </>
   )
+}
+
+
+function ButtonChange(props) {
+
+  function ChangeHandler() {
+    props.setValue(props.Value + 1);
+
+  }
+
+
+  return (
+    <div>
+
+      <button onClick={ChangeHandler} >Count {props.Value}</button>
+
+    </div>
+  );
+
+
 }
 
 export default App
---
+++ class5.1/vite-project/src/Component/Todo.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+
+const Todo = () => {
+
+const [todos, setTodos ] = useState([
+    {
+        tittle : "How are you",
+        description : "this not right",
+        number : 2342342
+    },
+    {
+        tittle : "How are you",
+        description : "this not right",
+        number : 2342342
+    },
+    {
+        tittle : "How are you",
+        description : "this not right",
+        number : 2342342
+    }
+
+])
+
+
+
+
+
+  return (
+    <div>
+
+
+
+
+    </div>
+  )
+}
+
+export default Todo
